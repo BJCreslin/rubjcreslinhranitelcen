55d6c2563e6d04a61804964bd2b2002479d6d7b1
bjcreslin <bjcreslin@gmail.com>
2019-03-05 17:34:26 +0700

    added

--- src/main/java/ru/bjcreslin/controller/ReadXLSController.java
+++ src/main/java/ru/bjcreslin/controller/ReadXLSController.java
@@ -1,5 +1,43 @@
 package ru.bjcreslin.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
+@Controller
 public class ReadXLSController {
     //todo сделать загрузку файла через нет
+    @RequestMapping(value = "/upload", method = RequestMethod.GET)
+    public
+    String provideUploadInfo() {
+        return "uploadForm";
+    }
+
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public @ResponseBody String handleFileUpload(@RequestParam("name") String name,
+                            @RequestParam("file") MultipartFile file) {
+        if (!file.isEmpty()) {
+            try {
+                byte[] bytes = file.getBytes();
+                BufferedOutputStream stream =
+                        new BufferedOutputStream(new FileOutputStream(new File(name + "-uploaded")));
+                stream.write(bytes);
+                stream.close();
+                return "Вы удачно загрузили " + name + " в " + name + "-uploaded !";
+            } catch (Exception e) {
+                return "Вам не удалось загрузить " + name + " => " + e.getMessage();
+            }
+        } else {
+            return "Вам не удалось загрузить " + name + " потому что файл пустой.";
+        }
+    }
+
 }
