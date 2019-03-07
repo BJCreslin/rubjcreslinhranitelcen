d984208e38262c941e2e7783ed2876cc923b9680
bjcreslin <bjcreslin@gmail.com>
2019-03-07 16:29:08 +0700

    added

--- src/main/java/ru/bjcreslin/repository/ItemRepository.java
+++ src/main/java/ru/bjcreslin/repository/ItemRepository.java
@@ -8,38 +8,38 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface ItemRepository extends CrudRepository<ItemEntity,Long> {
+public interface ItemRepository extends CrudRepository<ItemEntity, Long> {
 
 
-//    @Override
-//    <S extends ItemEntity> S save(S s);
-//
-//    @Override
-//    <S extends ItemEntity> Iterable<S> saveAll(Iterable<S> iterable);
-//
-//
-//
-//    @Override
-//    Optional<ItemEntity> findById(Long aLong);
-//
-//    ItemEntity findByName(String name);
-//
-//    ItemEntity findByAdress(String adress);
-//
-//    List<ItemEntity> findByGroupe(String groupe);
-//
-//    @Override
-//    List<ItemEntity> findAll();
-//
-//    @Override
-//    long count();
-//
-//    @Override
-//    void deleteById(Long aLong);
-//
-//    @Override
-//    void delete(ItemEntity itemEntity);
-//
-//    @Override
-//    void deleteAll();
+    @Override
+    <S extends ItemEntity> S save(S s);
+
+    @Override
+    <S extends ItemEntity> Iterable<S> saveAll(Iterable<S> iterable);
+
+    boolean existsItemEntityByName(String s);
+
+    @Override
+    Optional<ItemEntity> findById(Long aLong);
+
+    ItemEntity findByName(String name);
+
+    ItemEntity findByAdress(String adress);
+
+    List<ItemEntity> findByGroupe(String groupe);
+
+    @Override
+    List<ItemEntity> findAll();
+
+    @Override
+    long count();
+
+    @Override
+    void deleteById(Long aLong);
+
+    @Override
+    void delete(ItemEntity itemEntity);
+
+    @Override
+    void deleteAll();
 }
